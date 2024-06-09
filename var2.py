d8f28c5e4d95c2879855e0d286ae24c04454cf9e
GROZKROZ <167204193+GROZKROZ@users.noreply.github.com>
2024-06-10 00:54:49 +0700

    test

--- var2.py
+++ var2.py
@@ -66,6 +66,7 @@ def detect_collision(player_pos, enemy_pos):
     return False
 
 def show_game_over_screen():
+    print('hallo')
     screen.fill(black)
     game_over_font = pygame.font.SysFont("Arial", 30)
     game_over_text = game_over_font.render("Game Over!", True, white)
